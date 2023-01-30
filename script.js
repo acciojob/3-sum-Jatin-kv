33abc9d4f0a2d4cbecf47bef90635da19737b4ca
Jatin-kv <kjatin887@gmail.com>
2023-01-30 19:40:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
 function threeSum(arr, target) {
   //your code here
+  let i
+	let j
+	let k
+	const mindiff = []
+	for(i=0;i<arr.length;i++){
+		for(j=i+1;j<arr.length;j++){
+			for(k=j+1;k<arr.length;k++){
+				let sum = arr[i]+arr[j]+arr[k]
+				diff = sum - target
+				mindiff.push(Math.abs(diff))
+			}
+		}
+	}
+	let min = mindiff[0]
+	let p
+	for(p=1;p<mindiff.length;p++){
+	  if(min > mindiff[p]){
+	    min = mindiff[p]
+	  }
+	}
+	return min
 }
 
 module.exports = threeSum;
